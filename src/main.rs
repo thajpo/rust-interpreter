f67e67805600b2205c5a806b64ad841dafcf7ac4
Jason Pettinato <66739444+thajpo@users.noreply.github.com>
2026-01-09 17:02:27 -0500

    Working on tokenization

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,60 @@ fn main() {
 
         match lines.next() {
             Some(Ok(input)) => {
-                let tokens: Vec<&str> = input.split_whitespace().collect();
-                println!("{:?}", tokens)
+                let tokens = tokenize(&input);
+                println!("{:?}", tokens);
+                let interpreter_blocks = parse_tokens(tokens);
             }
             _ => break,
         }
+    }
+}
+
+/// Turns tokens into instruction steps for interpreter_blocks
+fn parse_tokens(tokens: Vec<String>) -> Vec<TokenType> {
+    // 1. if "(" then we will have an operation applied to all subsequent elements until closure
+    // 2. if ")" then the closest "("s operatoin no longer applies
+    for token in tokens {
+        if token = "( " {
+            
+        }
+        else if token = " )" {
+            
+        }
+        else if token {
+
+        }
+    }
+}
+
+/// Parses line from stdout and splits by whitespace
+fn tokenize(input: &str) -> Vec<String> {
+    input
+        .replace("(", "( ")
+        .replace(")", " )")
+        .replace(" (", "( ")
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect()
+}
+
+enum Expression {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tokenize_simple() {
+        let result = tokenize("(+ 1 2)");
+        assert_eq!(result, vec!["(", "+", "1", "2", ")"]);
+    }
+
+    #[test]
+    fn tokenize_nested() {
+        let result = tokenize("(+ 1 (* 2 3))");
+        assert_eq!(result, vec!["(", "+", "1", "(", "*", "2", "3", ")", ")"]);
     }
 }
