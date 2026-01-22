144efcda8ef1e0447e5b3a63d7b6a0a3790d4601
Jason Pettinato <66739444+thajpo@users.noreply.github.com>
2026-01-21 20:20:47 -0500

    Functions and variables nearly done.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,9 @@
 use std::collections::HashMap;
-use std::hash::Hash;
 use std::io;
 use std::io::Write;
 use std::str;
 
 const DEBUG: bool = false;
-
-// Defines limit of operations we can perform
-#[derive(Debug, PartialEq)]
-enum Operations {
-    Add,
-    Sub,
-    Mul,
-    Div,
-    FuncDef,
-}
-
-enum TokenKind {
-    StartExpr(String),
-    EndExpr(String),
-    Number(i64),
-    Operation(Operations),
-    Variable(String),
-    Function(String),
-}
-
-// Defines the information we store in the stack
-#[derive(Debug)]
-struct PartialNode {
-    var: Option<String>,
-    func: Option<String>,
-    op: Option<Operations>,
-    args: Vec<ExecutionNode>,
-}
-
-// Defines the tree structure we save. Stack pop becomes node
-#[derive(Debug)]
-enum ExecutionNode {
-    Number(i64),
-    Call(Operations, Vec<ExecutionNode>),
-    Variable(String),
-}
 
 fn main() {
     // Get latest on lines buffer
@@ -71,6 +34,135 @@ fn main() {
     }
 }
 
+enum TokenKind {
+    StartExpr,
+    EndExpr,
+    Number(i64),
+    Operation(Operations),
+    VariableDef,
+    FunctionDef,
+    Variable(String),
+}
+
+// Defines the tree structure we save. Stack pop becomes node
+#[derive(Debug)]
+enum ExecutionNode {
+    Number(i64),
+    Call(HeadKind, Vec<ExecutionNode>),
+    Let(String, Box<ExecutionNode>),
+    Def(String, Box<ExecutionNode>),
+    Variable(String),
+}
+
+// Defines limit of operations we can perform
+#[derive(Debug, PartialEq)]
+enum Operations {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+// Defines the information we store in the stack
+#[derive(Debug)]
+struct PartialNode {
+    head: Option<HeadKind>,
+    args: Vec<ExecutionNode>,
+}
+
+#[derive(Debug)]
+enum HeadKind {
+    BuiltIn(Operations),
+    Let,
+    Def,
+}
+
+/// Turns tokens into instruction steps for interpreter
+fn parse_tokens(tokens: Vec<String>) -> Result<ExecutionNode, String> {
+    let mut stack: Vec<PartialNode> = vec![];
+    let mut result: Option<ExecutionNode> = None;
+
+    for token in tokens {
+        let token_type = classify_token(&token);
+
+        match token_type {
+            TokenKind::StartExpr => {
+                stack.push(PartialNode {
+                    head: None,
+                    args: vec![],
+                });
+            }
+            TokenKind::EndExpr => {
+                let node = stack
+                    .pop()
+                    .ok_or("Err: Closing parentheses without open.")?;
+
+                let head = node.head.ok_or("Err: No head found.")?;
+                let new_node = ExecutionNode::Call(head, node.args);
+
+                if let Some(current) = stack.last_mut() {
+                    current.args.push(new_node);
+                } else {
+                    result = Some(new_node);
+                }
+            }
+            other => {
+                let current = stack
+                    .last_mut()
+                    .ok_or("Err: Tried accessing stack with nothing on it.")?;
+
+                match other {
+                    // if we see a number, push
+                    TokenKind::Number(n) => current.args.push(ExecutionNode::Number(n)),
+                    // if we see a 'def' set head
+                    TokenKind::FunctionDef => {
+                        current.head = Some(HeadKind::Def);
+                    }
+                    // if we see an op, set head
+                    TokenKind::Operation(op) => {
+                        current.head = Some(HeadKind::BuiltIn(op));
+                    }
+                    // if we see a 'let', set head
+                    TokenKind::VariableDef => {
+                        current.head = Some(HeadKind::Let);
+                    }
+                    TokenKind::Variable(var) => current.args.push(ExecutionNode::Variable(var)),
+                    _ => unreachable!(),
+                }
+            }
+        }
+    }
+    result.ok_or("Err: No closing parenthases.".to_string())
+}
+
+fn classify_token(token: &str) -> TokenKind {
+    if let Some(op) = parse_operations(token) {
+        TokenKind::Operation(op)
+    } else if let Ok(n) = token.parse::<i64>() {
+        TokenKind::Number(n)
+    } else if token == "(" {
+        TokenKind::StartExpr
+    } else if token == ")" {
+        TokenKind::EndExpr
+    } else if token == "let" {
+        TokenKind::VariableDef
+    } else if token == "def" {
+        TokenKind::FunctionDef
+    } else {
+        TokenKind::Variable(token.to_string())
+    }
+}
+
+fn parse_operations(s: &str) -> Option<Operations> {
+    match s {
+        "+" => Some(Operations::Add),
+        "-" => Some(Operations::Sub),
+        "*" => Some(Operations::Mul),
+        "/" => Some(Operations::Div),
+        _ => None,
+    }
+}
+
 struct Interpreter {
     vars: HashMap<String, i64>,
 }
@@ -87,35 +179,48 @@ impl Interpreter {
     fn set_var(&mut self, name: String, value: i64) {
         self.vars.insert(name, value);
     }
-    fn parse_and_evaluate_tree(&self, tree: ExecutionNode) -> i64 {
+    fn parse_and_evaluate_tree(&self, tree: ExecutionNode) -> Option<i64> {
         // tree has structure ExecutionNode {Number _or_ Call}
         // Call(op, <nodes>)
         // <nodes> can contain call objects
         // we need to go depth-wise into tree <node>, evaluate and work our way up
         match tree {
-            ExecutionNode::Number(n) => n,
-            ExecutionNode::Variable(var) => self.get_var(&var).copied().unwrap(),
-            ExecutionNode::Call(op, args) => {
+            // if number, just return it
+            ExecutionNode::Number(n) => Some(n),
+            // if var, then get it from hashmap
+            ExecutionNode::Variable(var) => Some(self.get_var(&var).copied().unwrap()),
+            // if call, then...
+            ExecutionNode::Call(head, args) => {
+                // values hold our nested call results (recursive calls)
                 let mut values: Vec<i64> = vec![];
                 if DEBUG {
                     println!("Args: {:?}", args);
                 }
+                // args are the children of the current node
+                // we need to evaluate each child
                 for child in args {
                     if DEBUG {
                         println!("Child: {:?}", child);
                     }
-                    values.push(self.parse_and_evaluate_tree(child));
+                    values.push(self.parse_and_evaluate_tree(child).unwrap());
                 }
-                match op {
-                    Operations::Add => values.iter().sum(),
-                    Operations::Mul => values.iter().product(),
-                    Operations::Sub => values[0] - values[1],
-                    Operations::Div => values[0] / values[1],
-                    Operations::FuncDef => {
-                        // If our operation is to define a function, we need to tell the
-                        todo!()
-                    }
+                // once we have all child values, we can operate on them with the head
+                match head {
+                    HeadKind::BuiltIn(Operations::Add) => Some(values.iter().sum()),
+                    HeadKind::BuiltIn(Operations::Mul) => Some(values.iter().product()),
+                    HeadKind::BuiltIn(Operations::Sub) => Some(values[0] - values[1]),
+                    HeadKind::BuiltIn(Operations::Div) => Some(values[0] / values[1]),
                 }
+            }
+            ExecutionNode::Let(name, value) => {
+                let value = self.parse_and_evaluate_tree(*value).unwrap();
+                self.set_var(name, value);
+                None
+            }
+            ExecutionNode::Def(name, value) => {
+                let value = self.parse_and_evaluate_tree(*value).unwrap();
+                self.set_var(name, value);
+                None
             }
         }
     }
@@ -129,86 +234,6 @@ fn tokenize(input: &str) -> Vec<String> {
         .split_whitespace()
         .map(|s| s.to_string())
         .collect()
-}
-
-/// Turns tokens into instruction steps for interpreter
-fn parse_tokens(tokens: Vec<String>) -> Result<ExecutionNode, String> {
-    let mut stack: Vec<PartialNode> = vec![];
-    let mut result: Option<ExecutionNode> = None;
-
-    for token in tokens {
-        let token_type = classify_token(&token);
-
-        match token_type {
-            TokenKind::StartExpr(_) => {
-                stack.push(PartialNode {
-                    var: None,
-                    func: None,
-                    op: None,
-                    args: vec![],
-                });
-            }
-            TokenKind::EndExpr(_) => {
-                let node = stack
-                    .pop()
-                    .ok_or("Err: Closing parentheses without open.")?;
-
-                let complete = match (node.op, node.var) {
-                    (Some(op), _) => ExecutionNode::Call(op, node.args),
-                    (_, Some(var)) => ExecutionNode::Variable(var),
-                    (None, None) => {
-                        return Err("Err: No operation or variable provided.".to_string());
-                    }
-                };
-
-                // check : are we done?
-                if let Some(parent) = stack.last_mut() {
-                    parent.args.push(complete);
-                } else {
-                    result = Some(complete);
-                }
-            }
-            other => {
-                let current = stack
-                    .last_mut()
-                    .ok_or("Err: Tried accessing stack with nothing on it.")?;
-
-                match other {
-                    TokenKind::Number(n) => current.args.push(ExecutionNode::Number(n)),
-                    TokenKind::Function(func) => current.func = Some(func),
-                    TokenKind::Operation(op) => current.op = Some(op),
-                    TokenKind::Variable(var) => current.var = Some(var),
-                    _ => unreachable!(),
-                }
-            }
-        }
-    }
-    result.ok_or("Err: No closing parenthases.".to_string())
-}
-
-fn classify_token(token: &str) -> TokenKind {
-    if let Some(op) = parse_operations(token) {
-        TokenKind::Operation(op)
-    } else if let Ok(n) = token.parse::<i64>() {
-        TokenKind::Number(n)
-    } else if token == "(" {
-        TokenKind::StartExpr(token.to_string())
-    } else if token == ")" {
-        TokenKind::EndExpr(token.to_string())
-    } else {
-        TokenKind::Variable(token.to_string())
-    }
-}
-
-fn parse_operations(s: &str) -> Option<Operations> {
-    match s {
-        "+" => Some(Operations::Add),
-        "-" => Some(Operations::Sub),
-        "*" => Some(Operations::Mul),
-        "/" => Some(Operations::Div),
-        "let" => Some(Operations::FuncDef),
-        _ => None,
-    }
 }
 
 #[cfg(test)]
